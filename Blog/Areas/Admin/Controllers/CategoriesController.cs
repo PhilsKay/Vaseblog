1f7ea7076fc6de3dd02bd8a1e689a5fcae6f2457
PhilsKay <philipinhokayode@gmail.com>
2022-07-01 08:46:10 +0100

    dashboard cont

--- Blog/Areas/Admin/Controllers/CategoriesController.cs
+++ Blog/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,9 +1,14 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoriesController : Controller
     {
         private readonly Context _contect;
@@ -20,6 +25,42 @@ namespace Blog.Areas.Admin.Controllers
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Add")]
+        public async Task<IActionResult> AddCategory(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                await _contect.Category.AddAsync(obj);
+                await _contect.SaveChangesAsync();
+                TempData["AddCategory"] = "Category saved Successfully";
+                return View();
+            }
+            ModelState.AddModelError(obj.CategoryName,"Must be in correct format");
+            return View(obj);   
+        }
+
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id != null)
+            {
+                var category = await _contect.Category.FindAsync(id); 
+                if(category != null)
+                {
+                    _contect.Category.Remove(category);
+                    _contect.SaveChanges();
+                    TempData["DeleteCategory"] = "Category deleted Successfully";
+                    return RedirectToAction("CategoryList");   
+                }
+                TempData["DeleteCategory"] = "Category cannot be deleted";
+                return RedirectToAction("CategoryList");
+
+            }
+            return NotFound();
         }
     }
 }
