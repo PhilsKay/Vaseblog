32c73cad30a6093f4c07833ddbbbaf9ba4e3762a
PhilsKay <philipinhokayode@gmail.com>
2022-07-14 03:17:26 +0100

    blog post cont

--- Blog/Controllers/BlogContentController.cs
+++ Blog/Controllers/BlogContentController.cs
@@ -1,19 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Blog.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     public class BlogContentController : Controller
     {
         private readonly ILogger<BlogContentController> _logger;
+        private readonly Context _context;
 
-        public BlogContentController(ILogger<BlogContentController> logger)
+        public BlogContentController(ILogger<BlogContentController> logger,Context context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult data()
+        public async Task<IActionResult> data(Guid? id)
         {
-            return View();
+            // confirm if the id is not empty
+            if(id == Guid.Empty)
+            {
+                return NotFound();
+            }
+            //check if the id is in the database
+            var check = await _context.BlogData.Where(c => c.BlogId == id).FirstOrDefaultAsync();
+            if(check != null)
+            {
+                var getCategoryName = await _context.Category.Where(c => c.CategoryId == check.CategoryId).FirstOrDefaultAsync();                //show the contents in view
+                ViewBag.Category = getCategoryName.CategoryName;
+                return View(check);
+            }
+            return NotFound();
         }
     }
 }
