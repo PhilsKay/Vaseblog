b3ad8d5f0c07a0058e09f7460f493e82de85b036
PhilsKay <philipinhokayode@gmail.com>
2022-09-19 02:30:43 +0100

    blog finalizing

---
+++ Blog/Helpers/ConnectionHelper.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+
+namespace Blog.Helpers
+{
+    public static class ConnectionHelper
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+
+        }
+
+        // build the connection string from the environment i.e Heroku
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Password = userInfo[0],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+            return builder.ToString();   
+
+        }
+    }
+}
---
+++ Blog/Helpers/DataHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Helpers
+{
+    public static class DataHelper
+    {
+         public static async Task MaanageDataAsync(IServiceProvider serviceProvider)
+        {
+            //service: an instance of db context
+            var dbContextSvc = serviceProvider.GetRequiredService<DbContext>();
+
+            //Migration: This is the programatic equivalent to Update-Database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
