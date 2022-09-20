18c922e0b5f1f4aca18ec412d4382698347cbd25
PhilsKay <philipinhokayode@gmail.com>
2022-09-20 18:49:54 +0100

    project d

---
+++ Migrations/20220920174629_heroku1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    public partial class heroku1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
